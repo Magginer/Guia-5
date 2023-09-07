885eac02220b16b72385d881f8559a299dd8b3dc
Javier M. Colombini <110408141+Khalraak@users.noreply.github.com>
2023-09-07 12:27:48 -0300

    inscr
    cion

--- Gestion univers/src/gestion/univers/Inscripcion.java
+++ Gestion univers/src/gestion/univers/Inscripcion.java
@@ -11,5 +11,69 @@ package gestion.univers;
  */
 public class Inscripcion {
   
-            
+    private int idInscripcion;
+    private Alumno alumno;
+    private Materia materia;
+    private double nota;
+
+    public Inscripcion() {
+    }
+
+    public Inscripcion(Alumno alumno, Materia materia, double nota) {
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
+
+    public Inscripcion(int idInscripcion, Alumno alumno, Materia materia, double nota) {
+        this.idInscripcion = idInscripcion;
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
+
+    public Inscripcion(double nota) {
+        this.nota = nota;
+    }
+
+    public int getidInscripcion() {
+        return idInscripcion;
+    }
+
+    public void setidInscripcion(int idInscripcion) {
+        this.idInscripcion = idInscripcion;
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    @Override
+    public String toString() {
+        String inscr= idInscripcion +" "+alumno.getApellido()+" "+alumno.getNombre()+" "+materia.getNombre();
+        return inscr;
+    }
+    
+    
+    
 }
