620398772c0bd4be56a3d0693e636b7e20c3c26a
Magginer <martin.novaresi@gmail.com>
2023-09-07 11:39:01 -0300

    asd

---
+++ Gestion univers/src/gestion/univers/Alumno.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestion.univers;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author marti
+ */
+public class Alumno {
+ private LocalDate fechadenacimiento;
+ private String nombre;
+ private String apellido;
+ private int IdAlumno;
+ private int dni;
+ private boolean estado;
+
+    public LocalDate getFechadenacimiento() {
+        return fechadenacimiento;
+    }
+
+    public void setFechadenacimiento(LocalDate fechadenacimiento) {
+        this.fechadenacimiento = fechadenacimiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getIdAlumno() {
+        return IdAlumno;
+    }
+
+    public void setIdAlumno(int IdAlumno) {
+        this.IdAlumno = IdAlumno;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" + "fechadenacimiento=" + fechadenacimiento + ", nombre=" + nombre + ", apellido=" + apellido + ", IdAlumno=" + IdAlumno + ", dni=" + dni + ", estado=" + estado + '}';
+    }
+ 
+}
---
+++ Gestion univers/src/gestion/univers/Inscripcion.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestion.univers;
+
+/**
+ *
+ * @author marti
+ */
+public class Inscripcion {
+    private String nombre;
+    private String Apellido
+    private int dni;
+    private localdate
+}
