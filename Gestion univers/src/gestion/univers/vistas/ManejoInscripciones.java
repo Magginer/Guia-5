3a6663217e7a92e11ce9cc9a5dfa206cf495328e
Magginer <martin.novaresi@gmail.com>
2023-09-26 13:20:04 -0300

    asd
    sdas

--- Gestion univers/src/gestion/univers/vistas/ManejoInscripciones.java
+++ Gestion univers/src/gestion/univers/vistas/ManejoInscripciones.java
@@ -7,11 +7,15 @@ package gestion.univers.vistas;
 
 import gestion.univers.accesoADatos.AlumnoData;
 import gestion.univers.accesoADatos.Conexion;
+import gestion.univers.accesoADatos.InscripcionData;
+import gestion.univers.accesoADatos.MateriaData;
 import gestion.univers.entidades.Alumno;
+import gestion.univers.entidades.Materia;
 import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -25,14 +29,19 @@ public class ManejoInscripciones extends javax.swing.JInternalFrame {
     
     Connection con = null;
     ArrayList Alista;
+    ArrayList<Materia> Mlista;
     AlumnoData alumno = new AlumnoData();
-    
+    MateriaData materia = new MateriaData();
+    DefaultTableModel modelo;
+   
     public ManejoInscripciones() {
         initComponents();
         
+        modelo = new DefaultTableModel();
         con = Conexion.getConexion();
         Alista = new ArrayList();
-        llenarcombo(); 
+        llenarcombo();
+        armaCabeceraTabla();
        
     }
 
@@ -45,8 +54,8 @@ public class ManejoInscripciones extends javax.swing.JInternalFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jScrollPane1 = new javax.swing.JScrollPane();
-        Tabladematerias = new javax.swing.JTable();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
@@ -56,54 +65,25 @@ public class ManejoInscripciones extends javax.swing.JInternalFrame {
         Inscribirboton = new javax.swing.JButton();
         Salirinscboton = new javax.swing.JButton();
         Anularinscboton = new javax.swing.JButton();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        Tabladematerias2 = new javax.swing.JTable();
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane2.setViewportView(jTable1);
 
         setClosable(true);
         setIconifiable(true);
         setResizable(true);
-
-        Tabladematerias.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null},
-                {null, null, null},
-                {null, null, null},
-                {null, null, null},
-                {null, null, null},
-                {null, null, null},
-                {null, null, null},
-                {null, null, null},
-                {null, null, null},
-                {null, null, null},
-                {null, null, null},
-                {null, null, null},
-                {null, null, null},
-                {null, null, null},
-                {null, null, null},
-                {null, null, null},
-                {null, null, null},
-                {null, null, null},
-                {null, null, null},
-                {null, null, null}
-            },
-            new String [] {
-                "ID", "Nombre", "Año"
-            }
-        ) {
-            Class[] types = new Class [] {
-                java.lang.Integer.class, java.lang.String.class, java.lang.Integer.class
-            };
-            boolean[] canEdit = new boolean [] {
-                false, false, false
-            };
-
-            public Class getColumnClass(int columnIndex) {
-                return types [columnIndex];
-            }
-
-            public boolean isCellEditable(int rowIndex, int columnIndex) {
-                return canEdit [columnIndex];
-            }
-        });
-        jScrollPane1.setViewportView(Tabladematerias);
 
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setText("Formulario de inscripcion");
@@ -117,6 +97,11 @@ public class ManejoInscripciones extends javax.swing.JInternalFrame {
         Materiainscriptaboton.setText("Materia inscripta");
 
         MateriaNoinscriptaboton.setText("Materia no inscripta");
+        MateriaNoinscriptaboton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                MateriaNoinscriptabotonActionPerformed(evt);
+            }
+        });
 
         Comboalumno.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -135,6 +120,19 @@ public class ManejoInscripciones extends javax.swing.JInternalFrame {
 
         Anularinscboton.setText("Anular inscripcion");
 
+        Tabladematerias2.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane3.setViewportView(Tabladematerias2);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -145,11 +143,6 @@ public class ManejoInscripciones extends javax.swing.JInternalFrame {
                         .addGap(116, 116, 116)
                         .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 162, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(21, 21, 21)
-                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(47, 47, 47)
-                        .addComponent(Comboalumno, javax.swing.GroupLayout.PREFERRED_SIZE, 226, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
                         .addGap(119, 119, 119)
                         .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
@@ -158,16 +151,21 @@ public class ManejoInscripciones extends javax.swing.JInternalFrame {
                         .addGap(49, 49, 49)
                         .addComponent(MateriaNoinscriptaboton))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(30, 30, 30)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 352, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 418, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(47, 47, 47)
+                                .addComponent(Comboalumno, javax.swing.GroupLayout.PREFERRED_SIZE, 226, javax.swing.GroupLayout.PREFERRED_SIZE))))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(43, 43, 43)
+                        .addContainerGap()
                         .addComponent(Inscribirboton)
                         .addGap(18, 18, 18)
                         .addComponent(Anularinscboton)
-                        .addGap(18, 18, 18)
+                        .addGap(28, 28, 28)
                         .addComponent(Salirinscboton)))
-                .addContainerGap(63, Short.MAX_VALUE))
+                .addContainerGap(35, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -186,14 +184,14 @@ public class ManejoInscripciones extends javax.swing.JInternalFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(Materiainscriptaboton)
                     .addComponent(MateriaNoinscriptaboton))
-                .addGap(32, 32, 32)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 211, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(34, 34, 34)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(Inscribirboton)
                     .addComponent(Anularinscboton)
                     .addComponent(Salirinscboton))
-                .addContainerGap(66, Short.MAX_VALUE))
+                .addContainerGap(136, Short.MAX_VALUE))
         );
 
         pack();
@@ -211,8 +209,53 @@ public class ManejoInscripciones extends javax.swing.JInternalFrame {
        }
         
     }
-    
-    
+      public void armaCabeceraTabla() {
+
+        //Titulos de Columnas
+        ArrayList<Object> columnas = new ArrayList<Object>();
+        columnas.add("ID");
+        columnas.add("Nombre");
+        columnas.add("Año");
+
+        for (Object it : columnas) {
+
+            modelo.addColumn(it);
+        }
+        Tabladematerias2.setModel(modelo);
+
+    }
+    public void llenartabla() {
+        InscripcionData inscripcion = new InscripcionData();
+
+        Alumno alu = (Alumno) Comboalumno.getSelectedItem();
+        Mlista = (ArrayList) inscripcion.ObtenerInscripcionesPorAlumno(alu.getIdAlumno());
+        for (Materia mate : Mlista) {
+            modelo.addRow(new Object[]{mate.getIdMateria(), mate.getNombre(), mate.getAnio()});
+
+        }
+    }
+
+    public void noinscripto() {
+
+        InscripcionData inscripcion = new InscripcionData();
+
+        Alumno alu = (Alumno) Comboalumno.getSelectedItem();
+        Mlista = (ArrayList) inscripcion.ObtenerMateriasNoCursadas(alu.getIdAlumno());
+        for (Materia mate : Mlista) {
+            modelo.addRow(new Object[]{mate.getIdMateria(), mate.getNombre(), mate.getAnio()});
+
+        }
+
+    }
+public void borraFilasTabla() {
+
+        int a = modelo.getRowCount() - 1;
+
+        for (int i = a; i >= 0; i--) {
+
+            modelo.removeRow(i);
+        }
+    }
     
     
     private void ComboalumnoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ComboalumnoActionPerformed
@@ -226,6 +269,13 @@ public class ManejoInscripciones extends javax.swing.JInternalFrame {
         
     }//GEN-LAST:event_SalirinscbotonActionPerformed
 
+    private void MateriaNoinscriptabotonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_MateriaNoinscriptabotonActionPerformed
+        // TODO add your handling code here:
+        borraFilasTabla();
+        noinscripto();
+        //asd
+    }//GEN-LAST:event_MateriaNoinscriptabotonActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton Anularinscboton;
@@ -234,10 +284,12 @@ public class ManejoInscripciones extends javax.swing.JInternalFrame {
     private javax.swing.JRadioButton MateriaNoinscriptaboton;
     private javax.swing.JRadioButton Materiainscriptaboton;
     private javax.swing.JButton Salirinscboton;
-    private javax.swing.JTable Tabladematerias;
+    private javax.swing.JTable Tabladematerias2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
 }
